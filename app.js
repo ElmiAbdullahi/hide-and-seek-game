cd2d69106bbc77de66989bb59157132c1ef11631
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-07-25 18:04:08 -0700

    finshed game

--- app.js
+++ app.js
@@ -16,28 +16,55 @@ const hidingPlaces = ['tree', 'shed', 'boulder'];
 
 let correctGuesses = 0;
 let totalGuesses = 0;
+let incorrectGuesses = 0;
 
 shedButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'shed');
+    console.log('clicked on shed');
+    
 });
 
 treeButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'tree');
+    console.log('clicked on tree');
+    
 });
 
 boulderButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'boulder');
+    console.log('clicked on boulder');
+    
 });
 
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
+    shedContainer.classList.remove('face');
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
     // then increment the guesses
+    if (userGuess === correctSpot) {
+        console.log('You got it!');
+        correctGuesses++;
+        totalGuesses++;
+        winsEl.textContent = correctGuesses;
+
+    }
+    else {
+        console.log('Sorry...maybe next time');
+        incorrectGuesses++;
+        totalGuesses++;
+        lossesEl.textContent = incorrectGuesses;
+
+    }
+    const correctEl = document.getElementById(correctSpot + '-container');
+    correctEl.classList.add('face');
+    totalEl.textContent = totalGuesses; 
     // then grab the appropriate container element for the correct guess from the DOM
     // then add the face class to that element so that the face shows up
     // then if the user guess is correct, increment the correct guesses
